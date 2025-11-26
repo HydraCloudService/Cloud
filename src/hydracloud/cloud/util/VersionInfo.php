69c750538080b2bee4afd6a7107b378adadeb3aa
xxFLORII <heunflorian2001@gmail.com>
2025-11-26 22:00:49 +0100

    2.1.1 is next

--- src/hydracloud/cloud/util/VersionInfo.php
+++ src/hydracloud/cloud/util/VersionInfo.php
@@ -4,7 +4,7 @@ namespace hydracloud\cloud\util;
 
 final class VersionInfo {
 
-    public const VERSION = "2.1.0";
+    public const VERSION = "2.1.1";
     public const DEVELOPERS = ["xxFLORII"];
     public const BETA = false;
 
